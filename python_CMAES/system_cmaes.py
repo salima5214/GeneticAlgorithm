5f8f8f77456b4ada3c2104397a18b506ec46860d
salima5214 <mingming5214@gmail.com>
2022-02-02 20:14:39 +0800

    add config.yaml

--- python_CMAES/system_cmaes.py
+++ python_CMAES/system_cmaes.py
@@ -5,25 +5,73 @@ from ipdb import set_trace
 from sklearn.mixture import GaussianMixture
 import heapq
 from copy import deepcopy
+import math
+import yaml
 
+config = yaml.load(open('./config.yaml', 'r'), Loader=yaml.FullLoader)
 
-
+# hp settings
 NFE = 0
+dim = config['hp']['dim']
+generations = config['hp']['generations']
+hit = False
+my_bound_min = config['hp']['bound_min']
+my_bound_max = config['hp']['bound_max']
+my_bounds_list = []
+for i in range(dim):
+    my_bounds_list.append([my_bound_min, my_bound_max])
+my_bounds = np.array(my_bounds_list)
 
-def evaluate(point):
+np.ones(dim)
+my_mean = np.ones(dim)*2.5
+
+optimizer = CMA(mean=my_mean, bounds=my_bounds, sigma=0.5, n_max_resampling=1)
+
+def printInfo(task):
+    if task == 0: #  0: Sphere      
+        print("Sphere")
+    elif task == 1: #  1: Rosenbrock
+        print("Rosenbrock")
+    elif task == 2: #  2: Rastrigin
+        print("Rastrigin")
+    elif task == 3: #  3: Michalewicz
+        print("Michalewicz")
+    print("dim = {} ".format(config['hp']['dim']))
+
+task = config['op']['task']
+printInfo(task)
+
+def evaluate(point, task):
     global NFE
     NFE += 1
-    return (100*((point[2]-(point[1]**2))**2)+(1-point[1])**2)+(100*((point[1]-(point[0]**2))**2)+(1-point[0])**2)
+    fitness = 0
+    if task == 0: #  0: Sphere      
+        for i in range(0, len(point)):
+            fitness += (point[i]**2)
+        return fitness
+    elif task == 1: #  1: Rosenbrock
+        for i in range(0, len(point)-1):
+            fitness += (100*((point[i+1]-(point[i]**2))**2)+(1-point[i])**2)
+        return fitness
+    elif task == 2: #  2: Rastrigin
+        for i in range(0, len(point)):
+            fitness += ((point[i]**2)-10*math.cos(2*math.pi*point[i]))
+        fitness += 10*len(point)
+        return fitness
+    elif task == 3: #  3: Michalewicz
+        for i in range(0, len(point)):
+            fitness += (-1*(math.sin(point[i]))*math.pow(math.sin((i+1)*point[i]*point[i]/(math.pi)),20))
+        return fitness
 
-optimizer = CMA(mean=np.array([2.5,2.5,2.5]), bounds=np.array([[0,5],[0,5],[0,5]]), sigma=0.5, n_max_resampling=1)
-generations = 25
 
-sqrt = int(np.sqrt(generations))
-fig, axs = plt.subplots(sqrt, sqrt, num="CMA-ES", sharex=True, sharey=True)
-points = np.ndarray((generations, optimizer.population_size, 3))
-points_1 = np.ndarray((generations, optimizer.population_size, 3))
-points_2 = np.ndarray((generations, optimizer.population_size, 3))
-hit = False
+
+
+points = np.ndarray((generations, optimizer.population_size, config['hp']['dim']))
+points_1 = np.ndarray((generations, optimizer.population_size, config['hp']['dim']))
+points_2 = np.ndarray((generations, optimizer.population_size, config['hp']['dim']))
+
+
+
 
 for g in range(generations):
     if hit: break
@@ -34,7 +82,8 @@ for g in range(generations):
         for i in range(optimizer.population_size):
             point = optimizer.ask() # sample next chromosome   
             points[g, i] = point # record
-            score = evaluate(point) # evaluate fitness
+            # print(point)
+            score = evaluate(point, config['op']['task']) # evaluate fitness
             if score < 0.001 and not hit:
                 print("hit global optimal")
                 print("point = {}".format(point))
@@ -110,15 +159,22 @@ if not hit:
     # print("NFE = {}".format(NFE))
 
  
-# target = np.array([1, 1, 1])
-for i in range(generations):
-    ax = axs[i//sqrt, i%sqrt]
-    ax.scatter(*zip(*points_1[i]), c="b")
-    ax.scatter(*zip(*points_2[i]), c="r")
-    # ax.scatter(*target,c="r")
-    ax.set_xlim([0,5])
-    ax.set_ylim([0,5])
 
-plt.savefig('cmaes_test.png')
+
+
+
+# sqrt = int(np.sqrt(generations))
+# fig, axs = plt.subplots(sqrt, sqrt, num="CMA-ES", sharex=True, sharey=True)
+
+# target = np.array([1, 1, 1])
+# for i in range(generations):
+#     ax = axs[i//sqrt, i%sqrt]
+#     ax.scatter(*zip(*points_1[i]), c="b")
+#     ax.scatter(*zip(*points_2[i]), c="r")
+#     # ax.scatter(*target,c="r")
+#     ax.set_xlim([0,5])
+#     ax.set_ylim([0,5])
+
+# plt.savefig('cmaes_test.png')
 
 
