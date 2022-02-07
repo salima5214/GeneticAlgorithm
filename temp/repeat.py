207e676f3995785d1fb8ff6e603696fc29d6530b
salima5214 <mingming5214@gmail.com>
2022-02-07 23:14:38 +0800

    add RM()

--- temp/repeat.py
+++
@@ -1,4 +0,0 @@
-import os
-
-for i in range(3):
-    os.system("python3 ./system_cmaes.py {}".format(i))
--- temp/store/test.py
+++
@@ -1,7 +0,0 @@
-import heapq
-
-a = [10, 50, 2, 60, 55, 1]
-
-re2 = map(a.index, heapq.nlargest(3, a))
-
-print(list(re2))
